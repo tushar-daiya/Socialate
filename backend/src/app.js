1fb7bce50053f2eba9915396844e1bb3be4f9c30
Tushar Daiya <tushardaiya@outlook.com>
2024-02-04 12:41:21 +0530

    project updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.USER_FRONTEND_URL,process.env.NGO_FRONTEND_URL],
     credentials: true,
   })
 );
